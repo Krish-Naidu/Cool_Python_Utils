2b6dcfcf0efd22d1e9a7fa6607b8be2c61c63911
Krish-Naidu <krish.28.naidu@gmail.com>
2025-09-09 14:40:02 +0100

    ok

--- Goepy.py
+++ Goepy.py
@@ -1,12 +1,7 @@
-geolocator = Nominatim(user_agent="geo_app")
 from geopy.geocoders import Nominatim
 
-location_name = input("Enter a location to geocode:")
 geolocator = Nominatim(user_agent="geo_app")
-location = geolocator.geocode(location_name)
+location = geolocator.geocode("Eiffel Tower")
 
-if location:
-	print(" Address:", location.address)
-	print(" Coordinates:", (location.latitude, location.longitude))
-else:
-	print("Location not found")
+print(" Address:", location.address)
+print(" Coordinates:", (location.latitude, location.longitude))
