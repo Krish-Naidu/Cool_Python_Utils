5985e11fac52c6cd02e77a7eeabb742eee025243
Krish-Naidu <krish.28.naidu@gmail.com>
2025-09-09 14:29:33 +0100

    ok

---
+++ geocode_eiffel.py
@@ -0,0 +1,12 @@
+geolocator = Nominatim(user_agent="geo_app")
+from geopy.geocoders import Nominatim
+
+location_name = input("Enter a location to geocode: ")
+geolocator = Nominatim(user_agent="geo_app")
+location = geolocator.geocode(location_name)
+
+if location:
+	print(" Address:", location.address)
+	print(" Coordinates:", (location.latitude, location.longitude))
+else:
+	print("Location not found.")
